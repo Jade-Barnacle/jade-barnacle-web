5a4a7a6164dc8945f8fe830ce672bcde41cce99d
Amal Ahmed <amvl.xxo@gmail.com>
2024-10-23 13:52:40 -0400

    Catalog committed

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,30 @@
+const data = {
+    products: [
+        {
+          name: 'Shirt',
+          imageUrl: '/images/d1.jpg',
+          price: 60,
+          brand: 'Nike',
+          rating: 4.5,
+          numberOfReviews: 10,
+        },
+        {
+          name: 'Shirt',
+          imageUrl: '/images/d1.jpg',
+          price: 80,
+          brand: 'Nike',
+          rating: 4.0,
+          numberOfReviews: 8,
+        },
+        {
+          name: 'Shirt',
+          imageUrl: '/images/d1.jpg',
+          price: 60,
+          brand: 'Nike',
+          rating: 4.7,
+          numberOfReviews: 15,
+        },
+      ]
+    }
+  export default data;
+  
